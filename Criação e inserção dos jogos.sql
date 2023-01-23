924585fa4a8b47b4f36a70fd2d2d2aa5c82808b4
Rodrigo Nunes <120372045+runes21@users.noreply.github.com>
2023-01-23 10:26:03 +0000

    Criação da base de dados para os users, criação e inserção dos jogos e prints

---
+++ Criação e inserção dos jogos.sql
@@ -0,0 +1,25 @@
+
+use projeto;
+
+INSERT INTO jogos (nome, genero) VALUES ("Zork", "aventura");
+INSERT INTO jogos (nome, genero) VALUES ("Adventure", "aventura");
+INSERT INTO jogos (nome, genero) VALUES ("The Legend Of Zelda", "aventura");
+INSERT INTO jogos (nome, genero) VALUES ("Indiana Jones", "aventura");
+INSERT INTO jogos (nome, genero) VALUES ("Star control", "aventura");
+INSERT INTO jogos (nome, genero) VALUES ("Day of the tentacle", "aventura");
+INSERT INTO jogos (nome, genero) VALUES ("Space", "shootemup");
+INSERT INTO jogos (nome, genero) VALUES ("Asteroids", "shootemup");
+INSERT INTO jogos (nome, genero) VALUES ("Missile Command", "shootemup");
+INSERT INTO jogos (nome, genero) VALUES ("Ikaruga", "shootemup");
+INSERT INTO jogos (nome, genero) VALUES ("Mushihisema", "shootemup");
+INSERT INTO jogos (nome, genero) VALUES ("Sinemora", "shootemup");
+INSERT INTO jogos (nome, genero) VALUES ("Archon", "estrategia");
+INSERT INTO jogos (nome, genero) VALUES ("Oregon", "estrategia");
+INSERT INTO jogos (nome, genero) VALUES ("Civilization", "estrategia");
+INSERT INTO jogos (nome, genero) VALUES ("Tetris", "estrategia");
+INSERT INTO jogos (nome, genero) VALUES ("Company of Heroes", "estrategia");
+INSERT INTO jogos (nome, genero) VALUES ("Mario", "estrategia");
+
+
+
+
